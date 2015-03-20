7ba9e16f3c37558ab01619ee15995623ef804817
jeffwhaley <jeffwhaley@gmail.com>
2015-03-20 12:49:14 -0500

    Lesson 3b part way

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Jeff Whaley on 3/20/15.
+//  Copyright (c) 2015 Jeff Whaley. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlow(sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
